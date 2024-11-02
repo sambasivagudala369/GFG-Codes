93a2308c4f73f9be34e25229c7cb4fafd01ffcfc
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2024-11-02 19:41:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int d = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.subarraySum(nums, d);
+            // Print all elements in the result list
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println(); // Print a new line after the result
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int tar) {
+        // code here
+        HashMap<Integer, Integer> sumMap = new HashMap<>();
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if(sum == tar){
+                ans.add(1);
+                ans.add(i+1);
+                return ans;
+            }
+            
+            if(sumMap.containsKey(sum-tar)){
+                ans.add(sumMap.get(sum-tar)+1);
+                ans.add(i+1);
+                return ans;
+            }
+            
+            sumMap.put(sum, i+1);
+        }
+        ans.add(-1);
+        return ans;
+    }
+}
