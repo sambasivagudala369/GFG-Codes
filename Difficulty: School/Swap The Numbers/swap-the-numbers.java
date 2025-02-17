7a6112c94efe4ed2010b8fdb9b8a7086a3d11192
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-02-17 21:02:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            Solution oj = new Solution();
+            oj.swap(a, b);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public void swap(int a, int b) {
+        // code here
+        int temp;
+          temp=a;
+          a=b;
+          b=temp;
+
+        System.out.println(a + " " + b);
+    }
+}
