7348659202d877e4ec908181945b8389ad48a23f
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-06-12 22:26:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Functions I - Python/string-functions-i-python.py
@@ -0,0 +1,17 @@
+def trim(str):
+    return str.strip()
+
+
+def exists(str, x):
+    a = str.find(x)
+    return a 
+
+
+def titleIt(str):
+    l = str
+    return l.title()
+
+
+def casesSwap(str):
+    s = str.swapcase()
+    return s
